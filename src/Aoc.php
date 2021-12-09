3c65dc1ff17b1c56153e3962afef5757209864e2
helori <helori.lanos@gmail.com>
2021-12-09 17:10:53 +0100

    day 9

---
+++ src/Aoc.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Aoc;
+
+use Symfony\Component\VarDumper\VarDumper;
+use Illuminate\Support\Str;
+use Illuminate\Support\Arr;
+use Carbon\Carbon;
+
+abstract class Aoc
+{
+    protected $lines = [];
+
+    abstract protected function init();
+    abstract protected function runPart1();
+    abstract protected function runPart2();
+
+    public function __construct()
+    {
+        $filename = Str::lower(Str::after(static::class, 'Aoc\\'));
+        $this->lines = array_filter($this->fileLines($filename.'.txt'));
+        $this->init();
+    }
+
+    public function run()
+    {
+        $start = Carbon::now();
+        $result1 = $this->runPart1();
+        $secondsPart1 = $start->floatDiffInSeconds();
+
+        $start = Carbon::now();
+        $result2 = $this->runPart2();
+        $secondsPart2 = $start->floatDiffInSeconds();
+
+        return [
+            'result_part1' => $result1,
+            'result_part2' => $result2,
+            'seconds_part1' => $secondsPart1,
+            'seconds_part2' => $secondsPart2,
+        ];
+    }
+
+    protected function fileLines($filename)
+    {
+        $filepath = __DIR__.'/../files/'.$filename;
+        $content = fopen($filepath, 'r');
+        $values = [];
+
+        while(!feof($content))
+        {
+            $values[] = trim(fgets($content));
+        }
+        return $values;
+    }
+
+    protected function toInts(array $values)
+    {
+        return array_map(function($v){ return intVal($v); }, $values);
+    }
+
+    protected function increment(array &$values, int $inc = 1)
+    {
+        foreach($values as $i => $value)
+        {
+            $values[$i] = $values[$i] + $inc;
+        }
+    }
+
+        
+
+    protected function matrixRows(&$matrix)
+    {
+        return count($matrix);
+    }
+
+    protected function matrixCols(&$matrix)
+    {
+        return count($matrix[0]);
+    }
+
+    protected function readMatrix(&$matrix, $callback)
+    {
+        $rows = $this->matrixRows($matrix);
+        $cols = $this->matrixCols($matrix);
+
+        for($r=0; $r<$rows; ++$r)
+        {
+            for($c=0; $c<$cols; ++$c)
+            {
+                $callback($matrix[$r][$c], $r, $c, $rows, $cols);
+            }
+        }
+    }
+
+    protected function dump($value)
+    {
+        VarDumper::dump($value);
+    }
+
+    protected function dd($value)
+    {
+        VarDumper::dump($value);
+        exit;
+    }
+}
---
+++ src/Day9.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Aoc;
+
+use Illuminate\Support\Str;
+use Illuminate\Support\Arr;
+
+class Day9 extends Aoc
+{
+    protected $matrix = null;
+    protected $rows = null;
+    protected $cols = null;
+
+    protected $lowPoints = [];
+
+    protected $basins = [];
+    protected $currentBasin = 0;
+    protected $checkedPoints = [];
+
+    protected function init()
+    {
+        $this->matrix = array_filter($this->lines);
+        foreach($this->matrix as $i => $line){
+            $this->matrix[$i] = $this->toInts(str_split(trim($line)));
+        }
+        $this->rows = $this->matrixRows($this->matrix);
+        $this->cols = $this->matrixCols($this->matrix);
+    }
+
+    protected function runPart1()
+    {
+        $this->readMatrix($this->matrix, function($v, $r, $c)
+        {
+            $isLow = true;
+            if($r > 0){
+                $isLow &= ($v < $this->matrix[$r - 1][$c]);
+            }
+            if($r < $this->rows - 1){
+                $isLow &= ($v < $this->matrix[$r + 1][$c]);
+            }
+            if($c < $this->cols - 1){
+                $isLow &= ($v < $this->matrix[$r][$c + 1]);
+            }
+            if($c > 0){
+                $isLow &= ($v < $this->matrix[$r][$c - 1]);
+            }
+            
+            if($isLow){
+                $this->lowPoints[] = $v;
+            }
+        });
+
+        $this->increment($this->lowPoints, 1);
+
+        return array_sum($this->lowPoints);
+    }
+
+    protected function runPart2()
+    {
+        $this->readMatrix($this->matrix, function($value, $r, $c) {
+            $inNewBasin = $this->checkPoint($r, $c);
+            if($inNewBasin){
+                $this->basins[] = $this->currentBasin;
+                $this->currentBasin = 0;
+            }
+        });
+
+        $b = $this->basins;
+        rsort($b);
+
+        return $b[0] * $b[1] * $b[2];
+    }
+
+    protected function checkPoint($r, $c)
+    {
+        $checkKey = $r.'_'.$c;
+        if(!isset($this->checkedPoints[$checkKey]))
+        {
+            $this->checkedPoints[$checkKey] = true;
+            if($this->matrix[$r][$c] < 9){
+                ++$this->currentBasin;
+                $this->checkAround($r, $c, $this->rows, $this->cols);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    protected function checkAround($r, $c, $rows, $cols)
+    {
+        if($r > 0){
+            $this->checkPoint($r - 1, $c, $rows, $cols);
+        }
+        if($r < $rows - 1){
+            $this->checkPoint($r + 1, $c, $rows, $cols);
+        }
+        if($c < $cols - 1){
+            $this->checkPoint($r, $c + 1, $rows, $cols);
+        }
+        if($c > 0){
+            $this->checkPoint($r, $c - 1, $rows, $cols);
+        }
+    }
+}
