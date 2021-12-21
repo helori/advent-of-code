bae1d35d8a9b5384c66eee898ff4b89e9294c86b
helori <helori.lanos@gmail.com>
2021-12-21 10:16:20 +0100

    Day 21 part 2 not working

--- src/Day_21.php
+++ src/Day_21.php
@@ -34,12 +34,14 @@ class Day_21 extends Aoc
 
         while($this->player1Score < $winScore && $this->player2Score < $winScore)
         {
-            $this->player1At = $this->movePawn($this->player1At);
+            $spaces = $this->rollDice3();
+            $this->player1At = $this->movePawn($this->player1At, $spaces);
             $this->player1Score += $this->player1At;
 
             if($this->player1Score < $winScore)
             {
-                $this->player2At = $this->movePawn($this->player2At);
+                $spaces = $this->rollDice3();
+                $this->player2At = $this->movePawn($this->player2At, $spaces);
                 $this->player2Score += $this->player2At;
             }
         }
@@ -47,9 +49,8 @@ class Day_21 extends Aoc
         return ($this->player2Score * $this->diceRolled);
     }
 
-    protected function movePawn($position)
+    protected function movePawn($position, $spaces)
     {
-        $spaces = $this->rollDice3();
         $spaces = ($spaces % 10);
         $newPosition = ($position + $spaces) % 10;
         $newPosition = ($newPosition === 0) ? 10 : $newPosition;
@@ -78,6 +79,107 @@ class Day_21 extends Aoc
 
     protected function runPart2()
     {
-        return 0;
+        $universes = [
+            [
+                'p' => [
+                    [
+                        'at' => 4,
+                        'sc' => 0,
+                    ],
+                    [
+                        'at' => 8,
+                        'sc' => 0,
+                    ],
+                ],
+                'e' => false,
+            ],
+        ];
+
+        $winScore = 21;
+        $allEnded = false;
+
+        while(!$allEnded)
+        {
+            $universes = $this->rollQuanticDice($universes, 0);
+            $universes = $this->rollQuanticDice($universes, 0);
+            $universes = $this->rollQuanticDice($universes, 0);
+
+            $this->computeScores($universes, 0);
+            $allEnded = $this->checkEnded($universes);
+
+            if(!$allEnded)
+            {
+                $universes = $this->rollQuanticDice($universes, 1);
+                $universes = $this->rollQuanticDice($universes, 1);
+                $universes = $this->rollQuanticDice($universes, 1);
+
+                $this->computeScores($universes, 1);
+                $allEnded = $this->checkEnded($universes);
+            }
+        }
+
+        $winning1 = count(Arr::where($universes, function($universe){
+            return $universe['p'][0]['sc'] >= 21;
+        }));
+
+        $winning2 = count(Arr::where($universes, function($universe){
+            return $universe['p'][1]['sc'] >= 21;
+        }));
+
+        return ($winning1 > $winning2) ? $winning1 : $winning2;
+    }
+
+    protected function checkEnded(&$universes)
+    {
+        $ended = true;
+        foreach($universes as $universe)
+        {
+            $ended &= $universe['e'];
+        }
+        return $ended;
+    }
+
+    protected function rollQuanticDice(&$universes, $playerIdx)
+    {
+        $newUniverses = [];
+        foreach($universes as $universe)
+        {
+            if(!$universe['e'])
+            {
+                $newUniverses[] = $this->createUniverse($universe, 1, $playerIdx);
+                $newUniverses[] = $this->createUniverse($universe, 2, $playerIdx);
+                $newUniverses[] = $this->createUniverse($universe, 3, $playerIdx);
+            }
+            else
+            {
+                $newUniverses[] = $universe;
+            }
+        }
+        return $newUniverses;
+    }
+
+    protected function createUniverse($baseUniverse, $spaces, $playerIdx)
+    {
+        $at = $baseUniverse['p'][$playerIdx]['at'];
+        $at = $this->movePawn($at, $spaces);
+        
+        $universe = $baseUniverse;
+        $universe['p'][$playerIdx]['at'] = $at;
+
+        return $universe;
+    }
+
+    protected function computeScores(&$universes, $playerIdx)
+    {
+        foreach($universes as &$universe)
+        {
+            $this->computeScore($universe, $playerIdx);
+            $universe['e'] = ($universe['p'][$playerIdx]['sc'] >= 21);
+        }
+    }
+
+    protected function computeScore(&$universe, $playerIdx)
+    {
+        $universe['p'][$playerIdx]['sc'] += $universe['p'][$playerIdx]['at'];
     }
 }
