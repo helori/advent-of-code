cd7003a49d07dcf0887f8fb65454947075446d57
Helori <helori.lanos@gmail.com>
2023-12-09 16:27:33 +0100

    Update Day_09.php

--- src/Aoc2023/Day_09.php
+++ src/Aoc2023/Day_09.php
@@ -8,102 +8,72 @@ use Illuminate\Support\Arr;
 
 class Day_09 extends Aoc
 {
+    protected $parts = [];
+
     protected function init()
     {
-        
+        $this->parts = [];
+
+        foreach($this->lines as $line)
+        {
+            $values = $this->toInts(explode(' ', trim($line)));
+            $sequences = [ $values ];
+
+            while(!$this->allZeros($values))
+            {
+                $seq = [];
+                for($i=0; $i<count($values)-1; ++$i)
+                {
+                    $seq[] = $values[$i+1] - $values[$i];
+                }
+                $values = $seq;
+                $sequences[] = $seq;
+            }
+            $this->parts[] = $sequences;
+        }
     }
 
     protected function runPart1()
     {
         $sum = 0;
 
-        foreach($this->lines as $line)
+        foreach($this->parts as $s)
         {
-            $values = $this->toInts(explode(' ', trim($line)));
-            $sequences = [ $values ];
+            $s[count($s) - 1][] = 0;
 
-            $isNeg = $values[0] < 0;
-
-            while(!$this->allZeros($values))
+            for($i=count($s)-2; $i>=0; --$i)
             {
-                $values = $this->sequenceFrom($values);
-                $sequences[] = $values;
+                $lastValNextSeq = $s[$i+1][count($s[$i+1]) - 1];
+                $s[$i][] = $lastValNextSeq + $s[$i][count($s[$i]) - 1];
             }
 
-            $sequences[count($sequences) - 1][] = 0;
-
-            for($i=count($sequences)-2; $i>=0; --$i)
-            {
-                $lastValNextSeq = $sequences[$i+1][count($sequences[$i+1]) - 1];
-                $sequences[$i][] = $lastValNextSeq + $sequences[$i][count($sequences[$i]) - 1];
-            }
-
-            $extrapolated = $sequences[0][count($sequences[0]) - 1];
-
-            if($isNeg){
-                $this->renderMatrix($sequences, ' ');
-                dump($extrapolated);
-            }
-
-            $sum += $extrapolated;
+            $sum += $s[0][count($s[0]) - 1];
         }
         return $sum;
-    }
-
-    protected function allZeros($values)
-    {
-        $zeros = array_filter($values, function($v){
-            return $v === 0;
-        });
-        return count($zeros) === count($values);
-    }
-    
-    protected function sequenceFrom($values)
-    {
-        $seq = [];
-        for($i=0; $i<count($values)-1; ++$i)
-        {
-            $seq[] = $values[$i+1] - $values[$i];
-        }
-        return $seq;
     }
 
     protected function runPart2()
     {
         $sum = 0;
 
-        foreach($this->lines as $line)
+        foreach($this->parts as $s)
         {
-            $values = $this->toInts(explode(' ', trim($line)));
-            $sequences = [ $values ];
+            array_unshift($s[count($s) - 1], 0);
 
-            $isNeg = true; //$values[0] < 0;
-
-            while(!$this->allZeros($values))
+            for($i=count($s)-2; $i>=0; --$i)
             {
-                $values = $this->sequenceFrom($values);
-                $sequences[] = $values;
+                $firstValNextSeq = $s[$i+1][0];
+                array_unshift($s[$i], $s[$i][0] - $firstValNextSeq);
             }
 
-            $this->renderMatrix($sequences, ' ');
-
-            array_unshift($sequences[count($sequences) - 1], 0);
-
-            for($i=count($sequences)-2; $i>=0; --$i)
-            {
-                $firstValNextSeq = $sequences[$i+1][0];
-                array_unshift($sequences[$i], $sequences[$i][0] - $firstValNextSeq);
-            }
-
-            $extrapolated = $sequences[0][0];
-
-            if($isNeg){
-                $this->renderMatrix($sequences, ' ');
-                dump($extrapolated);
-            }
-
-            $sum += $extrapolated;
+            $sum += $s[0][0];
         }
         return $sum;
+    }
+
+    protected function allZeros($values)
+    {
+        $counts = array_count_values($values);
+        return isset($counts[0]) && ($counts[0] === count($values));
     }
 }
